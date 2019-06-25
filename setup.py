b08e3b7af83780416ebe4d120b3517c86ea7cf5e
Jonas Van Malder <jonas.vanmalder@openanalytics.eu>
2019-06-25 12:32:42 +0200

    ready for pip

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+  name = 'pyrtfdom',
+  packages = ['pyrtfdom'],
+  version = '1.0',
+  license='gpl-3.0',
+  description = 'Parses RTF documents into a DOM-like structure',
+  author = 'https://github.com/crankycyclops',
+  url = 'https://github.com/jvanmalder/pyrtfdom',
+  keywords = ['RTF', 'DOM'],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GPL 3.0 License',
+    'Programming Language :: Python :: 3'
+  ],
+)
